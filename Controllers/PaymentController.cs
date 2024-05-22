b2d94729cd420ec4e59b065298b9d7e6cb44afb0
Onestyle7 <klusbartek8@gmail.com>
2024-05-22 21:02:30 +0200

    Added Cancel and Success view of payment and added functionality to PaymentController from documentation to CreateCheckout

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,10 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 
 namespace EcommerceApp.Controllers
 {
     public class PaymentController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCheckout()
+        {
+            var domain = "https://localhost:7095";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    Price = "{{PRICE_ID}}",
+                    Quantity = 1,
+                  },
+                },
+                Mode = "payment",
+                SuccessUrl = domain + "/Payment/Success",
+                CancelUrl = domain + "/Payment/Cancel",
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+        public IActionResult Succes()
+        {
+            return View();
+        }
+        public IActionResult Cancel()
         {
             return View();
         }
