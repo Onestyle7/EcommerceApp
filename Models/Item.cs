a71b7815c7ef79c740b04c2377b4c92b9d38f12d
Onestyle7 <klusbartek8@gmail.com>
2024-05-23 19:19:06 +0200

    Added Item Model

---
+++ Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace EcommerceApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImgUrl { get; set; }
+        public string PriceId { get; set; }
+
+    }
+}
